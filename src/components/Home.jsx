b5316953a6ad7b2b9fc355d3032588405f571a41
Ananda Sau <anandasau707@gmail.com>
2024-07-10 20:37:34 +0530

    pre deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,46 @@
 // rafce
-import React from 'react'
-import "./Home.css"
-import Button from './Button'
-import linkedin from "../images/linkedin-logo-24.png"
-import github from "../images//github.png"
-import twitter from "../images//twitter-logo-24.png"
-import download from "../images//download.png" ;
-import {faDownload} from '@fortawesome/free-solid-svg-icons'
-
+import React from "react";
+import "./Home.css";
+import Button from "./Button";
+import linkedin from "../images/linkedin-logo-24.png";
+import github from "../images//github.png";
+import twitter from "../images//twitter-logo-24.png";
+import download from "../images//download.png";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <div>
-        <div className="HomePage" id='Home__page'> 
-            <div className="textArea">
+      <div className="HomePage" id="Home__page">
+        <div className="textArea">
+          <h1> MERN Developer </h1>
+          <p>
+            {" "}
+            Hi, I'm Ananda , A passionate Full Stack Web Developer based in
+            Kolkata , India. 📍{" "}
+          </p>
 
-                <h1>    MERN Developer  </h1>
-                <p> Hi, I'm ABC. A passionate Full Stack Web Developer based in Kolkata , India. 📍 </p>
+          <div className="social_media">
+            <a href="https://www.linkedin.com/in/ananda-sau/" target="_black">
+              <img src={linkedin} alt="linkedin" />
+            </a>
+            <a href="https://github.com/Ananda-2" target="_black">
+              <img src={github} alt="github" />
+            </a>
+            <a href="https://twitter.com/hexagon_02" target="_black">
+              <img src={twitter} alt="twitter" />
+            </a>
+          </div>
 
-                <div className="social_media">
-                  <a href="https://www.linkedin.com/in/ananda-sau/" target='_black'><img src={linkedin} alt = 'linkedin'/></a>
-                  <a href="https://github.com/Ananda-2" target='_black'><img src={github} alt = 'github'/></a>
-                  <a href="https://twitter.com/hexagon_02" target='_black'><img src={twitter} alt = 'twitter'/></a>
-                </div>
-
-                <Button name = "Download CV" icon = {faDownload}/>
-
-            </div>
-
-            <div className="photoArea">
-              {/* <div className="profile_picture"></div> */}
-            </div>
+          <Button name="Download CV" icon={faDownload} />
         </div>
-    </div>
-  )
-}
 
-export default Home
+        <div className="photoArea">
+          {/* <div className="profile_picture"></div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,135 +1,125 @@
-import React from 'react';
-import './Projects.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCirclePlay} from '@fortawesome/free-regular-svg-icons'
-import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import "./Projects.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 const data = [
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/shopsphere.png",
+    title: "ShopSphere",
+    desc: "an end-to-end website for client use, similar to e-commerce platforms",
+    Github__Link: "https://github.com/Ananda-2/E-commerce-Project",
+    Live__demo:
+      "https://e-commerce-project-3blp-hve26qy6k-anandas-projects-91946f99.vercel.app/",
   },
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/AI Image Gen.png",
+    title: "AI Image Generator",
+    desc: "an AI Image Generator utilizing MERN stack in conjunction with the OpenAI API .",
+    Github__Link: "https://github.com/Ananda-2/AI-Image-Generator",
+    Live__demo: "https://ananda-ai-image-generator.netlify.app/",
   },
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/blockchain.jpg",
+    title: "Decentralized Blood Supply",
+    desc: " a decentralized blood supply system on blockchain using Next Js, enabling dynamic management of donors,suppliers, hospitals, and real-time blood distribution to meet patient needs",
+    Github__Link: "https://github.com/Ananda-2/decentralized-blood-supply",
+    Live__demo: "",
   },
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/Thread.png",
+    title: "Thread",
+    desc: "Thread Clone",
+    Github__Link: "https://github.com/Ananda-2/Thread-app",
+    Live__demo: "",
   },
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/css event page.png",
+    title: "CSS NIT Durgapur",
+    desc: "Official website of Computer Science Students society  , made on react Tailwind",
+    Github__Link: "https://github.com/Ananda-2/CSS-Official-Website",
+    Live__demo: "https://www.cssnitdgp.in/event",
   },
   {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
+    img: "../../public/project_images/Train reservation.png",
+    title: "Train Reservation System",
+    desc: " Train Reservation System manager with the concept of Object Oriented Programing conjunction with Operating System concept And Socket Io",
+    Github: "https://github.com/Ananda-2/Train-Management-System",
+    Live__demo: "",
   },
-  {
-    "img" : '../../public/project_images/img-1.jpg' ,
-    "title" : "Project Title" ,
-    "desc" :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut",
-    "Github__icon" : {faGithub} ,
-    "Live__demo" : {faCirclePlay} ,
-  }
-
-] ;
-
+];
 
 // All Project Items using map
 
-const all__projects = data.map((each__project,i) => 
+const all__projects = data.map((each__project, i) => (
   <SwiperSlide key={i}>
-    <img src = {each__project.img} alt='photo'/>
+    <img src={each__project.img} className="project__image" alt="photo" />
 
     <div className="details">
       <h1 className="project__title">{each__project.title}</h1>
       <p className="project__desc">{each__project.desc}</p>
-      <div className='project__icon'>
-        <a href="http://"><FontAwesomeIcon  icon= {faGithub} /></a>
-        <a href="http://"><FontAwesomeIcon  icon= {faCirclePlay} /></a>
-
-    </div>
+      <div className="project__icon">
+        <a href={each__project.Github__Link}>
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href={each__project.Live__demo}>
+          <FontAwesomeIcon icon={faCirclePlay} />
+        </a>
+      </div>
     </div>
   </SwiperSlide>
-)
-
-
-
-
+));
 
 function Projects() {
   return (
-    <div className = 'Project__div' id='Project__page'>
-    <div className="container">
-      <h1 className="heading">Recent Projects</h1>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        {all__projects}
+    <div className="Project__div" id="Project__page">
+      <div className="container">
+        <h1 className="heading">Recent Projects</h1>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+        >
+          {all__projects}
 
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
           </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          <div className="swiper-pagination"></div>
-        </div>
-      </Swiper>
-    </div>
+        </Swiper>
+      </div>
     </div>
   );
 }
 
 export default Projects;
-
